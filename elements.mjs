29f0d5b17de8bb2f61fb2bccd51f71b357f6afce
aklnk <aklnk@users.noreply.github.com>
2018-11-13 15:31:53 +0100

    working example

---
+++ elements.mjs
@@ -0,0 +1,82 @@
+var elements= {
+        H : { key:"H",  index: 1 },
+        He: { key:"He", index: 2 },
+        Li: { key:"Li", index: 3 },
+        Be: { key:"Be", index: 4 },
+        B : { key:"B",  index: 5 },
+        C : { key:"C",  index: 6 },
+        N : { key:"N",  index: 7 },
+        O : { key:"O",  index: 8 },
+        F : { key:"F",  index: 9 },
+        Ne: { key:"Ne", index: 10 },
+        Na: { key:"Na", index: 11 },
+        Mg: { key:"Mg", index: 12 },
+        Al: { key:"Al", index: 13 },
+        Si: { key:"Si", index: 14 },
+        P : { key:"P",  index: 15 },
+        S : { key:"S",  index: 16 },
+        Cl: { key:"Cl", index: 17 },
+        Ar: { key:"Ar", index: 18 },
+        K : { key:"K",  index: 19 },
+        Ca: { key:"Ca", index: 20 },
+        Sc: { key:"Sc", index: 21 },
+        Ti: { key:"Ti", index: 22 },
+        V : { key:"V",  index: 23 },
+        Cr: { key:"Cr", index: 24 },
+        Mn: { key:"Mn", index: 25 },
+        Fe: { key:"Fe", index: 26 },
+        Co: { key:"Co", index: 27 },
+        Ni: { key:"Ni", index: 28 },
+        Cu: { key:"Cu", index: 29 },
+        Zn: { key:"Zn", index: 30 },
+        Ga: { key:"Ga", index: 31 },
+        Ge: { key:"Ge", index: 32 },
+        As: { key:"As", index: 33 },
+        Se: { key:"Se", index: 34 },
+        Br: { key:"Br", index: 35 },
+        Kr: { key:"Kr", index: 36 }
+    };
+
+   
+// export function addTextToBody(text) {
+//     const div = document.createElement('div');
+//     div.textContent = text;
+//     document.body.appendChild(div);
+// }    
+
+export function fillElements(){
+    const div = document.createElement('div');
+    div.textContent = "test text";
+    document.body.appendChild(div);
+
+    var sel =  document.myform.selectElement;
+    var zones = ""
+    //var data = Object.keys(elements);
+    // var len = data.length();
+    for (var prop in elements){
+        console.log("obj." + prop + " = " + elements[prop]);
+        zones += "<option>" + prop + "</option>";
+    }
+    // for (i=0; i < data.length(); i++) {
+    //     //zones += "<option>" + "5" + "</option>";
+    //     a=5;
+    // }
+    sel.innerHTML =  zones; //"<option>" + data[6] + "</option>";
+
+}
+//  const testtest = "import test";
+
+// export default elements;
+
+// // Default export
+// export default () => {
+//     let greetings = `Hi from the default export!`
+//     console.log(greetings);
+    
+//     return greetings;
+//   };
+  
+//   // Named export `doStuff`
+//   export const doStuff = () => {
+//     console.log('Doing stuff…');
+//   };
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+
+
+//function fillElements(){
+//    var sel = document.getElementsByName["selectElement"];
+//    sel.options[sel.options.length] = new Option("test test", 1)
+//    alert("Hello, World1");
+//}
+
 function Hello() {
     alert("Hello, World1");
- }
+}
