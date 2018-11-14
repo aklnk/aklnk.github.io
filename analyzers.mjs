8285e19786fec92840c6dfae75423edba1bd66f4
aklnk <aklnk@users.noreply.github.com>
2018-11-14 18:42:39 +0100

    partly working

--- analyzers.mjs
+++ analyzers.mjs
@@ -1,45 +1,48 @@
 
-export const analyzers= {
+export const crystals= {
     Si : { key:"Si",  index: 1, a: 5.4307 },
     Ge: { key:"Ge", index: 2, a: 5.658 },
-    Quarz: { key:"Quarz", index: 3 },
+    Quarz : { key:"Quarz", index: 3 }
 };
 
-export const reflections= {
-    Si111 : { key:"Si111", crystal: analyzers.Si, h: 1, k: 1, l: 1, main: True },
-    Si333 : { key:"Si333", crystal: analyzers.Si, h: 3, k: 3, l: 3, main: False },
-    Si444 : { key:"Si444", crystal: analyzers.Si, h: 4, k: 4, l: 4, main: False },
-    Si555 : { key:"Si555", crystal: analyzers.Si, h: 5, k: 5, l: 5, main: False },
-    Si777 : { key:"Si777", crystal: analyzers.Si, h: 7, k: 7, l: 7, main: False },
-    Si888 : { key:"Si888", crystal: analyzers.Si, h: 8, k: 8, l: 8, main: False },
-    Si999 : { key:"Si999", crystal: analyzers.Si, h: 9, k: 9, l: 9, main: False },
+export const  analyzers= {
+    Si111 : { key:"Si111", crystal: "Si", h: 1, k: 1, l: 1, main: 1 },
+    // Si333 : { key:"Si333", crystal: "Si", h: 3, k: 3, l: 3, main: 0 },
+    // Si444 : { key:"Si444", crystal: "Si", h: 4, k: 4, l: 4, main: 0 },
+    // Si555 : { key:"Si555", crystal: "Si", h: 5, k: 5, l: 5, main: 0 },
+    // Si777 : { key:"Si777", crystal: "Si", h: 7, k: 7, l: 7, main: 0 },
+    // Si888 : { key:"Si888", crystal: "Si", h: 8, k: 8, l: 8, main: 0 },
+    // Si999 : { key:"Si999", crystal: "Si", h: 9, k: 9, l: 9, main: 0 },
 
-    Si220 : { key:"Si220", crystal: analyzers.Si, h: 2, k: 2, l: 0, main: True },
-    Si440 : { key:"Si440", crystal: analyzers.Si, h: 4, k: 4, l: 0, main: False },
-    Si660 : { key:"Si660", crystal: analyzers.Si, h: 6, k: 6, l: 0, main: False },
-    Si880 : { key:"Si880", crystal: analyzers.Si, h: 8, k: 8, l: 0, main: False },
+    Si220 : { key:"Si220", crystal: "Si", h: 2, k: 2, l: 0, main: 1 },
+    // Si440 : { key:"Si440", crystal: "Si", h: 4, k: 4, l: 0, main: 0 },
+    // Si660 : { key:"Si660", crystal: "Si", h: 6, k: 6, l: 0, main: 0 },
+    // Si880 : { key:"Si880", crystal: "Si", h: 8, k: 8, l: 0, main: 0 },
 
-    Si400 : { key:"Si400", crystal: analyzers.Si, h: 4, k: 0, l: 0, main: True },
-    Si800 : { key:"Si800", crystal: analyzers.Si, h: 8, k: 0, l: 0, main: False },
+    Si400 : { key:"Si400", crystal: "Si", h: 4, k: 0, l: 0, main: 1 },
+    // Si800 : { key:"Si800", crystal: "Si", h: 8, k: 0, l: 0, main: 0 },
 
-    Si311 : { key:"Si311", crystal: analyzers.Si, h: 3, k: 1, l: 1, main: True },
-    Si933 : { key:"Si933", crystal: analyzers.Si, h: 9, k: 3, l: 3, main: False },
+    Si311 : { key:"Si311", crystal: "Si", h: 3, k: 1, l: 1, main: 1 },
+    // Si933 : { key:"Si933", crystal: "Si", h: 9, k: 3, l: 3, main: 0 },
 
-    Si331 : { key:"Si331", crystal: analyzers.Si, h: 3, k: 3, l: 1, main: True },
-    Si331 : { key:"Si331", crystal: analyzers.Si, h: 3, k: 3, l: 1, main: False },
+    Si331 : { key:"Si331", crystal: "Si", h: 3, k: 3, l: 1, main: 1 },
+    // Si993 : { key:"Si993", crystal: "Si", h: 9, k: 9, l: 3, main: 0 },
 
-    Si531 : { key:"Si531", crystal: analyzers.Si, h: 5, k: 3, l: 1,  main: True },
+    Si422 : { key:"Si422", crystal: "Si", h: 4, k: 2, l: 2, main: 1 },
+    // Si844 : { key:"Si422", crystal: "Si", h: 8, k: 4, l: 4, main: 0 },
 
-    Si553 : { key:"Si553", crystal: analyzers.Si, h: 5, k: 5, l: 3, main: True },
+    Si531 : { key:"Si531", crystal: "Si", h: 5, k: 3, l: 1,  main: 1 },
+
+    Si553 : { key:"Si553", crystal: "Si", h: 5, k: 5, l: 3, main: 1 },
 
 
-    Ge111 : { key:"Ge111", crystal: analyzers.Ge, h: 1, k: 1, l: 1 },
-    Ge333 : { key:"Ge333", crystal: analyzers.Ge, h: 3, k: 3, l: 3 },
-    Ge444 : { key:"Ge444", crystal: analyzers.Ge, h: 4, k: 4, l: 4 },
-    Ge555 : { key:"Ge555", crystal: analyzers.Ge, h: 5, k: 5, l: 5 },
-    Ge777 : { key:"Ge777", crystal: analyzers.Ge, h: 7, k: 7, l: 7 },
-    Ge888 : { key:"Ge888", crystal: analyzers.Ge, h: 8, k: 8, l: 8 },
-    Ge999 : { key:"Ge999", crystal: analyzers.Ge, h: 9, k: 9, l: 9 },
+    Ge111 : { key:"Ge111", crystal: "Ge", h: 1, k: 1, l: 1, main : 1 },
+    // Ge333 : { key:"Ge333", crystal: "Ge", h: 3, k: 3, l: 3, main : 0 },
+    // Ge444 : { key:"Ge444", crystal: "Ge", h: 4, k: 4, l: 4, main : 0 },
+    // Ge555 : { key:"Ge555", crystal: "Ge", h: 5, k: 5, l: 5, main : 0 },
+    // Ge777 : { key:"Ge777", crystal: "Ge", h: 7, k: 7, l: 7, main : 0 },
+    // Ge888 : { key:"Ge888", crystal: "Ge", h: 8, k: 8, l: 8, main : 0 },
+    // Ge999 : { key:"Ge999", crystal: "Ge", h: 9, k: 9, l: 9, main : 0 },
 
-    Quarz: { key:"Quarz", index: 3 },
+    Quarz: { key:"Quarz", index: 3 }
 };
--- script.js
+++ script.js
@@ -7,6 +7,90 @@
 //    alert("Hello, World1");
 //}
 
+//import {emission} from './emission.mjs';
+
 function Hello() {
     alert("Hello, World1");
+}
+
+function fillEmission(selectedElement){
+    var sel =  document.myform.selectElement;
+    var ind = sel.selectedIndex+1;
+    var data = emission[ind]
+    var zones = ""
+    for (var prop in data){
+        zones += "<option>" + prop + " " + data[prop].e + " eV (" + data[prop].int +   ") </option>";
+    }
+    var sel =  document.myform.selectEmission;
+            sel.innerHTML =  zones;
+
+}
+
+function calculateBraggAngles(){
+    var lstRefl = createDSpacesList();
+    var selEl =  document.myform.selectElement;
+    var selEm =  document.myform.selectEmission;
+    var indEl = selEl.selectedIndex+1;
+    var indEm = selEm.selectedIndex;
+    var theta = 0;
+    var theta2 = 0;
+    var energy = emission[indEl][Object.keys(emission[indEl])[indEm]].e;
+    var wlength = 1239.84187 / energy;
+    var best = 0;
+    var best2 = 0;
+    for (var r=0; r<lstRefl.length;r++){
+        var dspacing = lstRefl[r].d;
+        if (wlength / (2*dspacing/10) > 1) continue;
+        try{
+            var angle = Math.asin(wlength / (2*dspacing/10)) * (180 / Math.PI);
+        }
+        catch(err){ var a =5; }
+        if (angle < 90){                
+            if (angle > theta){  
+                theta2 = theta;
+                best2 = best;            
+                theta = angle;
+                best = r;
+                if(theta2==0){
+                    theta2 = theta;
+                }
+            }
+            if ( (angle>theta2) && (angle < theta) ){
+                theta2 = angle;
+                best2 = r;
+            }
+        }
+    }
+    text = "Element " + elements[selEl].symbol;
+    //text += "Emission\t" + Object.keys(emission[indEl])[indEm] + " " + emission[indEl][Object.keys(emission[indEl])[indEm]].e +" eV";
+    var resultTextArea = document.myform.resultArea;
+    resultTextArea.innerHTML = text;
+
+}
+
+function createDSpacesList(){
+    var group = document.myform.analyzerGroup;
+    var listOfReflections = [];
+    var maxOrder = 10;
+    for (var i=0; i<group.length; i++){                 
+        if (group[i].checked == true){ // process all checked crystals 
+            for(var j=1;j<=maxOrder;j++){ //check all available reflections till 9th order
+                var h = analyzers[group[i].id].h * j;
+                var k = analyzers[group[i].id].k * j;
+                var l = analyzers[group[i].id].l * j;
+                if( ( (h+k+l) % 4 == 0) || ( (h+k+l) % 2 == 1) ){ //check if reflection is allowed
+                    //calculate d
+                    var d = crystals[analyzers[group[i].id].crystal].a / Math.sqrt(h*h + k*k + l*l);
+                    var refl = {};
+                    refl['crystal'] = analyzers[group[i].id].crystal;
+                    refl['h'] = h;
+                    refl['k'] = k;
+                    refl['l'] = l;
+                    refl['d'] = d;
+                    listOfReflections.push(refl);
+                }
+            }
+        }
+    }
+    return listOfReflections;
 }
